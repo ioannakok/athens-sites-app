55961579e606fabbb8e9e704616aa7a94be3003f
Ioanna Kokkini <ioannakok@hotmail.com>
2021-10-29 19:29:50 +0100

    Add dummy data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,31 @@
-function AllMeetupsPage() { 
-    return <div>All Meetups Page</div>;
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is the first meetup",
+    image: "",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description: "This is the first meetup",
+  },
+  {
+    id: "m2",
+    title: "This is the second meetup",
+    image: "",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description: "This is the first meetup",
+  },
+];
+
+function AllMeetupsPage() {
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <ul>
+        {DUMMY_DATA.map((meetup) => {
+          return <li key={meetup.id}>{meetup.title}</li>;
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
