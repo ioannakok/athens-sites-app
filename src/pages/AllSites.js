7bf43dcce91071f41f2a4820c501c2ccacd4b38c
Ioanna Kokkini <ioannakok@hotmail.com>
2021-10-29 19:43:06 +0100

    Modify data

--- src/pages/AllSites.js
+++ src/pages/AllSites.js
@@ -1,17 +1,27 @@
 const DUMMY_DATA = [
   {
     id: "m1",
-    title: "This is the first meetup",
-    image: "",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is the first meetup",
+    title: "Acropolis of Athens",
+    image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRMeuQYtLEdOR3KrJtD2Let6dSMNMgn4FpoDmgHEu1BmVI5Il9MHoQqnlmMeJ801CCR0NyXA1dTcWEaUK1qnLXuLw",
+    description: "5th-century B.C. hilltop temple monument",
   },
   {
     id: "m2",
-    title: "This is the second meetup",
-    image: "",
-    address: "Meetupstreet 5, 12345 Meetup City",
-    description: "This is the first meetup",
+    title: "Acropolis Museum",
+    image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQJsnGcc3EORg8VctkXb6kD-DODt7WX89uD_YWMldEr3wdAkaUG-CwIxpNlhAWv-n63HmNUTvX_JPfutHNiz6t2VA",
+    description: "Ancient treasures in contemporary venue",
+  },
+  {
+    id: "m3",
+    title: "Temple of Olympian Zeus",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROPDo1NjIIPKTyu8vP4v0G7zixBWfNYRR-jw741KwIKtwjuMn3Sf8BoKFKsLH9J92DxvX-Q2nfgR_71HzqC0z7IQ",
+    description: "Ruined temple completed under Hadrian",
+  },
+  {
+    id: "m4",
+    title: "Ancient Agora of Athens",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJqQhR0DrhXpik2AR5QOPHiUMmlkspLV6pPacHaUSeAXJ2WnIYmSlIJ8oWpguCNSWtPuDtLSMjwl0VwGZa0ent3Q",
+    description: "Iconic Greek ruins with a museum",
   },
 ];
 
