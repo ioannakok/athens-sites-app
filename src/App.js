00dc79998c031c97f419bd0139d1080d45fa370e
Ioanna Kokkini <ioannakok@hotmail.com>
2021-10-29 18:44:03 +0100

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import FavouritesPage from "./pages/Favourites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favourites">
+          <FavouritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
