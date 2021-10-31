b0e6214810abd19c4f6916f42d01ad4e42e1f4c5
Ioanna Kokkini <ioannakok@hotmail.com>
2021-10-31 20:16:57 +0000

    Add form to add new site

---
+++ src/components/layout/sites/NewSiteForm.js
@@ -0,0 +1,56 @@
+import { useRef } from 'react';
+import Card from "../../ui/Card";
+import classes from './NewSiteForm.module.css';
+
+
+function NewSiteForm(props) {
+
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+    function submitHandler(event) {
+        event.preventDefault();
+
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
+        const enteredDescription = descriptionInputRef.current.value;
+
+        const siteData = {
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAddress,
+            description: enteredDescription    
+        };
+
+        console.log(siteData);
+    }
+
+    return <Card>
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+                <label htmlFor='title'>Title</label>
+                    <input type='text' required id='title' ref={titleInputRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='image'>Image</label>
+                    <input type='url' required id='image' ref={imageInputRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='address'>Address</label>
+                    <input type='text' required id='address' ref={addressInputRef} />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='description'>Description</label>
+                <textarea id="description" required rows='5' ref={descriptionInputRef} />
+            </div>
+            <div className={classes.action}>
+                <button>Add Site</button>
+            </div>
+        </form>
+    </Card>
+}
+
+export default NewSiteForm;
--- src/pages/NewSite.js
+++ src/pages/NewSite.js
@@ -1,5 +1,10 @@
+import NewSiteForm from "../components/layout/sites/NewSiteForm";
+
 function NewSite() { 
-    return <div>NewSite Page</div>;
+    return <section>
+        <h1>Add new site</h1>
+        <NewSiteForm />
+    </section>
 }
 
 export default NewSite;
